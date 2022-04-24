f917040eb8794177c0601d13e4db9449597a77aa
Amanda Dahm <demlenar@gmail.com>
2022-04-23 22:37:18 -0400

    add adv file

---
+++ adv.py
@@ -0,0 +1,11 @@
+import time
+
+def print_pause(string):
+    print(string)
+    time.sleep(.25)
+
+
+print_pause("You find yourself standing in an open field, filled with grass and "
+        "yellow wildflowers. ")
+print_pause("Rumor has it that a wicked fairie is somewhere around here, and has "
+        "been terrifying the nearby village.")
